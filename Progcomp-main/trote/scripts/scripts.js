6be819279ccc8667316249ff350d91e97f0a896a
Breno Batista Silva <brenomanubs@gmail.com>
2025-03-24 20:39:18 -0300

    java

--- Progcomp-main/trote/scripts/scripts.js
+++ Progcomp-main/trote/scripts/scripts.js
@@ -1,3 +1,11 @@
 function calcular(){
-    
+    //recupera o valor da ação social
+    // declaração váriavel sem tipo
+    let acaoSocial = document.getElementById("acaoSocial").value
+    //recupera o valor da homenagem digitado
+    let homenagem = document.getElementById("homenagem").value
+    //vamos somar os valores
+    let soma = Number(acaoSocial) + Number(homenagem)
+    alert(soma)
+
 }
