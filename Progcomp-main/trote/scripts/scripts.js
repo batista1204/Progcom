d462bd372ecf7ee0093696f949e65c26064ca7a3
Breno Batista Silva <brenomanubs@gmail.com>
2025-03-31 20:31:07 -0300

    java 3

--- Progcomp-main/trote/scripts/scripts.js
+++ Progcomp-main/trote/scripts/scripts.js
@@ -8,7 +8,26 @@ function calcular(){
     //recupera a quantidade de litros de leite
     let leite = document.getElementById("leite").value
 
-    let kit = document.getElementById("kit").value
+    let kit = Number(document.getElementById("kit").value)
+    let equipe = document.getElementById("equipe").value
+    let pontoskit = 0
+    if (equipe == "Laranja") {
+        if (kit >= 97) {
+            pontoskit = 5000 +((kit - 97) * 30)
+        }
+        else if(kit >= 78) {
+            pontoskit = 4000 +((kit - 78) *30)    
+        }
+        else if(kit >=49){
+            pontoskit = 2500 +((kit - 49) *30)
+        }    
+        else if(kit >=19){
+            pontoskit = 1000 + ((kit - 19) *30)
+        }
+        }
+    
+    alert(pontoskit)
+
 
     let suplemento = document.getElementById("suplemento").value
 
@@ -24,7 +43,13 @@ function calcular(){
 
     let oleo = document.getElementById("oleo").value
 
-    let soma = Number(acaoSocial) + Number(homenagem) + (2 * leite) + (30 * kit) + (15 * suplemento) + (5 * arroz) + Number(arroz1) + (2 * feijao) + Number(feijao1) + Number(macarrao / 2) + Number(oleo)
+    let pontuation = 0
+
+    if (kit >= 100) {
+        pontuation = 4000
+    }
+
+    let soma = Number(acaoSocial) + Number(homenagem) + (2 * leite) + (30 * kit) + (15 * suplemento) + (5 * arroz) + Number(arroz1) + (2 * feijao) + Number(feijao1) + Number(0.5 * macarrao) + Number(oleo)
     // devolve o resultado para o HTML
 
     document.getElementById("soma").innerHTML = soma.toFixed(2)
